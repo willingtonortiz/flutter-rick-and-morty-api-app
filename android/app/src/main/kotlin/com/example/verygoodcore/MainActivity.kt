f278c6d0a32dcf53c61b133f86b66463c543bcc4
willingtonortiz <willingtonortizmaurtua@hotmail.com>
2022-05-11 12:34:01 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.rick_and_morty_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:rick_and_morty_app/app/app.dart';
+import 'package:rick_and_morty_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
